6b76adb34762c1940fa2ec9d8b52c3a3660e7b2b
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-03-07 22:44:08 +0530

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -1,1 +1,19 @@
+// Abstract class representing a general shape
+public abstract class Shape {
+    protected String shapeName;
+
+    // Constructor to assign shape name
+    public Shape(String shapeName) {
+        this.shapeName = shapeName;
+    }
+
+    // Abstract methods that all shapes must implement
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+
+    // Display shape name when selected
+    public void displayShape() {
+        System.out.println("\nShape Selected: " + shapeName);
+    }
+}
 
