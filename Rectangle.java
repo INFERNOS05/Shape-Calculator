573fd534f1f1714cc9d1bc60e1edf95c0a79174d
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-03-07 22:43:51 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,21 @@
+public class Rectangle extends Shape {
+    private double length, width;
+
+    // Constructor to initialize rectangle dimensions
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width; // Area formula: length * width
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (length + width); // Perimeter formula: 2 * (length + width)
+    }
+}
 
