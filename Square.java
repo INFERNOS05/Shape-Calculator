d02380468e1409181ae9366d4befb5848c326be6
sehajdeep <155955772+INFERNOS05@users.noreply.github.com>
2025-03-07 22:44:44 +0530

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,1 +1,20 @@
+public class Square extends Shape {
+    private double side;
+
+    // Constructor to initialize square side length
+    public Square(double side) {
+        super("Square");
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side; // Area formula: side²
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 4 * side; // Perimeter formula: 4 * side
+    }
+}
 
